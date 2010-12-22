8bdbdaa3dea88958b485ef37cae2d397634a20b1
George Antoniadis <george@noodles.gr>
2010-12-22 15:42:14 +0200

    Fixed php limit to match js one

--- grid.php
+++ grid.php
@@ -13,7 +13,7 @@ mysql_select_db($database) or die("Could not select database");
 
 // collect request parameters
 $start  = isset($_REQUEST['start'])  ? $_REQUEST['start']  :  0;
-$count  = isset($_REQUEST['limit'])  ? $_REQUEST['limit']  : 20;
+$count  = isset($_REQUEST['limit'])  ? $_REQUEST['limit']  : 50;
 $sort   = isset($_REQUEST['sort'])   ? $_REQUEST['sort']   : '';
 $dir    = isset($_REQUEST['dir'])    ? $_REQUEST['dir']    : 'ASC';
 
